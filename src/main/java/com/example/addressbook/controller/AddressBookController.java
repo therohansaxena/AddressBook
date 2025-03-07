661c23fe8b33b28bd835ff0175f3a64a652fe2b0
Rohan Saxena <therohansaxena@gmail.com>
2025-03-07 12:21:45 +0530

    UC3

--- src/main/java/com/example/addressbook/controller/AddressBookController.java
+++ src/main/java/com/example/addressbook/controller/AddressBookController.java
@@ -3,67 +3,43 @@ package com.example.addressbook.controller;
 import com.example.addressbook.dto.AddressBookDTO;
 import com.example.addressbook.dto.ResponseDTO;
 import com.example.addressbook.model.AddressBook;
-import com.example.addressbook.service.IAddressBookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/address-book")
 public class AddressBookController {
 
-    @Autowired
-    private IAddressBookService addressBookService;
-
     @GetMapping("/api/test")
     public String test() {
-        return "Test Successful! Welcome to Address Book Application";
+        return "Hello from Address Book Controller";
     }
 
-    @RequestMapping({"", "/", "/get"})
-    public ResponseEntity<ResponseDTO> getAllAddressBook() {
-        List<AddressBook> addressBookData = addressBookService.getAddressBookData();
-        return new ResponseEntity<>(new ResponseDTO("Get All Employees Payroll Data", addressBookData), HttpStatus.OK);
+    @GetMapping({"", "/", "/get"})
+    public ResponseEntity<ResponseDTO> getContact() {
+        AddressBookDTO addressBook = new AddressBookDTO();
+        return new ResponseEntity<>(new ResponseDTO("Get Contacts", addressBook), HttpStatus.OK);
     }
 
-    @GetMapping("/get/{id}")
-    public ResponseEntity<ResponseDTO> getContactById(@PathVariable Long id) {
-        try {
-            AddressBook addressBook = addressBookService.getAddressBookDataById(id);
-            return new ResponseEntity<>(new ResponseDTO("Get Call for ID Successful", addressBook), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(new ResponseDTO("Get Call for ID Unsuccessful", e.getMessage()), HttpStatus.NOT_FOUND);
-        }
+    @GetMapping("/get/{contactId}")
+    public ResponseEntity<ResponseDTO> getContactById() {
+        AddressBookDTO addressBook = new AddressBookDTO();
+        return new ResponseEntity<>(new ResponseDTO("Get Contact", addressBook), HttpStatus.OK);
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDTO> addInAddressBook(@RequestBody AddressBookDTO AddressBookDTO) {
-        AddressBook newAddressBook = addressBookService.createAddressBookData(AddressBookDTO);
-        return new ResponseEntity<>(new ResponseDTO("Create New Employee Payroll Data", newAddressBook), HttpStatus.OK);
+    public ResponseEntity<ResponseDTO> addContact(@RequestBody AddressBookDTO addressBookDTO) {
+        return new ResponseEntity<>(new ResponseDTO("Add New Contact", addressBookDTO), HttpStatus.OK);
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<ResponseDTO> updateAddressBook(@PathVariable Long id, @RequestBody AddressBookDTO updatedAddressBook) {
-        AddressBook addressBook = addressBookService.getAddressBookDataById(id);
-        boolean operation = addressBookService.updateAddressBookData(addressBook, updatedAddressBook);
-        if(!operation)
-            return new ResponseEntity<>(new ResponseDTO("Update Failed", addressBook), HttpStatus.NOT_MODIFIED);
-        return new ResponseEntity<>(new ResponseDTO("Updated Employee Payroll Data for:" + addressBook + "to below Data ", updatedAddressBook), HttpStatus.OK);
+    @PutMapping("/update/{contactId}")
+    public ResponseEntity<ResponseDTO> updateContact(@PathVariable Long contactId, @RequestBody AddressBookDTO addressBookDTO) {
+        return new ResponseEntity<>(new ResponseDTO("Update the Contact", addressBookDTO), HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<ResponseDTO> deleteAddressBook(@PathVariable Long id) {
-        try {
-            AddressBook addressBook = addressBookService.getAddressBookDataById(id);
-            addressBookService.deleteAddressBookData(id);
-            return new ResponseEntity<ResponseDTO>(new ResponseDTO("Deleted Employee Payroll Data for id: " + id, addressBook), HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<ResponseDTO>(new ResponseDTO("Employee Payroll Data for id " + id + " is not found", new AddressBookDTO()), HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            return new ResponseEntity<ResponseDTO>(new ResponseDTO("Error Deleting Employee Payroll Data for id: " + id, new AddressBookDTO()), HttpStatus.NOT_FOUND);
-        }
+    @DeleteMapping("/delete/{contactId}")
+    public ResponseEntity<ResponseDTO> deleteContact(@PathVariable Long contactId) {
+        return new ResponseEntity<ResponseDTO>(new ResponseDTO("Deleted the contact with id: " + contactId, new AddressBookDTO()), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/addressbook/model/AddressBook.java
+++ src/main/java/com/example/addressbook/model/AddressBook.java
@@ -1,6 +1,5 @@
 package com.example.addressbook.model;
 
-import com.example.addressbook.dto.AddressBookDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,10 +19,4 @@ public class AddressBook {
     private String name;
     private String address;
     private long phoneNumber;
-
-    public AddressBook(AddressBookDTO addressBookDTO) {
-        this.name = addressBookDTO.getName();
-        this.address = addressBookDTO.getAddress();
-        this.phoneNumber = addressBookDTO.getPhoneNumber();
-    }
 }
