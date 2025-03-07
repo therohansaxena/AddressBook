659d9f15dbecbec3905689eb7571556346f45b94
Rohan Saxena <therohansaxena@gmail.com>
2025-03-07 12:19:16 +0530

    UC1

--- src/main/java/com/example/addressbook/AddressBookApplication.java
+++ src/main/java/com/example/addressbook/AddressBookApplication.java
@@ -2,15 +2,16 @@ package com.example.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AddressBookApplication {
 
     public static void main(String[] args) {
-        Dotenv.configure()
-                .systemProperties()
-                .load();
         SpringApplication.run(AddressBookApplication.class, args);
     }
+
 }
+
