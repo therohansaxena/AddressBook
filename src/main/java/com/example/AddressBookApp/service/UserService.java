49b5764288553d39a643cc043dc0785cdc65f47e
Rohan Saxena <therohansaxena@gmail.com>
2025-03-21 15:36:42 +0530

    UC6_EMAIL_SERVICE_ON_REGISTRATION

--- src/main/java/com/example/AddressBookApp/service/UserService.java
+++ src/main/java/com/example/AddressBookApp/service/UserService.java
@@ -17,6 +17,9 @@ public class UserService implements IUserService {
     private UserRepository userRepository;
 
     @Autowired
+    private EmailService emailService;
+
+    @Autowired
     private JwtUtil jwtUtil;
 
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
@@ -31,6 +34,12 @@ public class UserService implements IUserService {
         user.setUsername(userdto.getName());
         user.setEmail(userdto.getEmail());
         user.setPassword(passwordEncoder.encode(userdto.getPassword())); // Encrypt password
+        String subject = "Welcome to Our Platform!";
+        String body = "<h1>Hello " + userdto.getName() + "!</h1>"
+                + "<p>Thank you for registering on our platform.</p>"
+                + "<p>We are excited to have you on board.</p>";
+
+        emailService.sendEmail(user.getEmail(), subject, body);
 
         userRepository.save(user);
         return "User registered successfully!";
